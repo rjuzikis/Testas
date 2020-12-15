7c444858cc8714fd1a73ffb14b7291b3cfc6d18d
rjuzikis <76051384+rjuzikis@users.noreply.github.com>
2020-12-15 15:39:37 +0200

    Add files via upload

---
+++ Pirma uzd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+          Console.WriteLine("Enter first letter: ");    
+          var pirma = Console.ReadLine();
+          Console.WriteLine("Enter second letter: ");
+          var antra = Console.ReadLine();
+          Console.WriteLine("Enter third letter: ");
+          var trecia = Console.ReadLine();
+          var allLetters = trecia + antra + pirma;  
+          Console.WriteLine(allLetters);
+        }
+      
+      
+          
+          
+
+        
+    }
+}
